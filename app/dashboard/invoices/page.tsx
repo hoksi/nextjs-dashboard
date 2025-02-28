ded1e07e083c43ca3f4f27406a563eab1304b0f8
한대승 <hoksi3k@gmail.com>
2025-02-28 16:14:00 +0900

    dashboard 추가

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function Invoices() {
+    return <p>Invoices 페이지</p>;
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+    // 대시보드 페이지 컴포넌트를 정의합니다.
+    return <p>대시보드 페이지</p>;
+}
